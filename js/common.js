013d5e7bb31e23b42b52f43d4e84d8e40cbfb309
Xsodex <flex513513@gmail.com>
2020-12-01 13:31:01 +0300

    Адаптивный второй блок

--- js/common.js
+++ js/common.js
@@ -1,69 +1,112 @@
-// $(document).ready(function() {https://clck.ru/F5MdE
-//    $('.menu_but').click(function()
-//    {
-//    	$('.nav_menu_list').slideToggle(350,function()
-//    		{
-//    			if($(this).css('display')==='none')
-// 				{
-// 					$(this).removeAttr('style');
-// 				}
-//    		});
-//    })
-// });
+$(document).ready(function () {
+	https: //clck.ru/F5MdE
+		$('.mobile_but').click(function () {
+			$('.header_menu').slideToggle(350, function () {
+				if ($(this).css('display') === 'none') {
+					$(this).removeAttr('style');
+				}
+			});
+		})
+});
 
 
 
 
-$(function()
-{
+$(function () {
 
 	var btnUp = $(".but_up");
 
 	var top = $(this).scrollTop();
-		if( top > 300 ){
-			btnUp.fadeIn(500);
-		}
-		else
-		{
-			btnUp.fadeOut(500);
-		}
+	if (top > 300) {
+		btnUp.fadeIn(500);
+	} else {
+		btnUp.fadeOut(500);
+	}
 
-		
-	$(document).on("scroll",function()
-	{
+
+	$(document).on("scroll", function () {
 		var top = $(this).scrollTop();
-		if( top > 300 ){
+		if (top > 300) {
 			btnUp.fadeIn(500);
-		}
-		else
-		{
+		} else {
 			btnUp.fadeOut(500);
 		}
 	});
 
-	btnUp.on("click",function()
-	{
+	btnUp.on("click", function () {
 		$("html,body").animate({
-			scrollTop:0
-		},400);
+			scrollTop: 0
+		}, 400);
 	});
 });
 
 
-$(".link").on("click",function(e)
-	{
-		e.preventDefault();
+$(".link").on("click", function (e) {
+	e.preventDefault();
 
-		var selector = $(this).attr("href"); //получили Атрибут
-		var h = $(selector);  //получили элемент с данным атрибутом
+	var selector = $(this).attr("href"); //получили Атрибут
+	var h = $(selector); //получили элемент с данным атрибутом
+
+
+	$("html,body").animate({
+		scrollTop: h.offset().top //мы скролим тег body на высоту h.offset(высоту получаем при клике на элемент меню).top
+	}, 500);
+});
+
+
+$(document).ready(function () {
+	new WOW().init();
+});
+
+
+$(".fa-bars-click").on('click', function () {
+	$('.fa-times-mod').css('display', 'block');
+	$(".fa-bars-click").css('display', 'none');
+	$('header .header_content h1').css('display', 'none');
+	$('header .header_content h4').css('display', 'none');
+});
+
+$(".fa-times-mod").on('click', function () {
+	$('.fa-times-mod').css('display', 'none');
+	$(".fa-bars-click").css('display', 'block');
+	$('header .header_content h1').css('display', 'block');
+	$('header .header_content h4').css('display', 'block');
+});
+
+
+$('.review_slider').slick({
+	centerMode: true,
+	centerPadding: '20px',
+	slidesToShow: 3,
+	dots: true,
+	infinite: true,
+
+	responsive: [
 		
-
-		$("html,body").animate({
-			scrollTop: h.offset().top //мы скролим тег body на высоту h.offset(высоту получаем при клике на элемент меню).top
-		},500);
-	});
-
-
-$(document).ready(function() {
-  new WOW().init();
+		{
+			breakpoint: 1280,
+			settings: {
+				arrows: false,
+				centerMode: true,
+				slidesToShow: 3
+			}
+		},
+		{
+			breakpoint: 1100,
+			settings: {
+				arrows: false,
+				centerMode: true,
+				slidesToShow: 2
+			}
+		},
+		{
+			breakpoint: 768,
+			settings: {
+				arrows: false,
+				centerMode: true,
+				slidesToShow: 1
+			}
+		},
+		
+	]
 });
--- js/dist/common.dev.js
+++ js/dist/common.dev.js
@@ -1,17 +1,15 @@
 "use strict";
 
-// $(document).ready(function() {https://clck.ru/F5MdE
-//    $('.menu_but').click(function()
-//    {
-//    	$('.nav_menu_list').slideToggle(350,function()
-//    		{
-//    			if($(this).css('display')==='none')
-// 				{
-// 					$(this).removeAttr('style');
-// 				}
-//    		});
-//    })
-// });
+$(document).ready(function () {
+  https: //clck.ru/F5MdE
+  $('.mobile_but').click(function () {
+    $('.header_menu').slideToggle(350, function () {
+      if ($(this).css('display') === 'none') {
+        $(this).removeAttr('style');
+      }
+    });
+  });
+});
 $(function () {
   var btnUp = $(".but_up");
   var top = $(this).scrollTop();
@@ -50,4 +48,45 @@ $(".link").on("click", function (e) {
 });
 $(document).ready(function () {
   new WOW().init();
+});
+$(".fa-bars-click").on('click', function () {
+  $('.fa-times-mod').css('display', 'block');
+  $(".fa-bars-click").css('display', 'none');
+  $('header .header_content h1').css('display', 'none');
+  $('header .header_content h4').css('display', 'none');
+});
+$(".fa-times-mod").on('click', function () {
+  $('.fa-times-mod').css('display', 'none');
+  $(".fa-bars-click").css('display', 'block');
+  $('header .header_content h1').css('display', 'block');
+  $('header .header_content h4').css('display', 'block');
+});
+$('.review_slider').slick({
+  centerMode: true,
+  centerPadding: '20px',
+  slidesToShow: 3,
+  dots: true,
+  infinite: true,
+  responsive: [{
+    breakpoint: 1280,
+    settings: {
+      arrows: false,
+      centerMode: true,
+      slidesToShow: 3
+    }
+  }, {
+    breakpoint: 1100,
+    settings: {
+      arrows: false,
+      centerMode: true,
+      slidesToShow: 2
+    }
+  }, {
+    breakpoint: 768,
+    settings: {
+      arrows: false,
+      centerMode: true,
+      slidesToShow: 1
+    }
+  }]
 });
